7200539073cb90482b9a37e473e0de3a00f14245
rhatrivianto <rhatrivianto@gmail.com>
2026-02-03 15:48:35 +0700

    delete unknown word

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,4 @@
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// // import jsconfigPaths from 'vite-jsconfig-paths' // Nonaktifkan sementara untuk debugging
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     // jsconfigPaths() // Nonaktifkan sementara
-//   ],
-//   build: {
-//     emptyOutDir: true,
-//   }
-// })
 
 // vite.config.js
 import { defineConfig } from 'vite'
@@ -20,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Pastikan ini ada
+  base: './',
   build: {
     outDir: 'dist',
     sourcemap: false
